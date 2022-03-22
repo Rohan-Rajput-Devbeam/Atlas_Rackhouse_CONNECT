efa71075a1282f92689637608b1bc94ad6e7decc
Rohan Rajput <Rohan.Rajput.atlassoft@BeamSuntory.com>
2022-03-22 03:32:51 -0700

    Configured Target Audience (User & Group)

--- src/webparts/pageCreator/PageCreatorWebPart.ts
+++ src/webparts/pageCreator/PageCreatorWebPart.ts
@@ -25,7 +25,7 @@ import * as strings from 'PageCreatorWebPartStrings';
 
 import "@pnp/sp/webs";
 import "@pnp/sp/items";
-import "@pnp/sp/folders"; 
+import "@pnp/sp/folders";
 import "@pnp/sp/lists";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
@@ -63,8 +63,11 @@ export interface IPageCreatorWebPartProps {
   PermanentTimeStamp: any;
   Executed: any;
   FolderUrl: any;
+  RackID: any;
+  delete: boolean;
 }
 import { IPropertyFieldGroupOrPerson } from "@pnp/spfx-property-controls/lib/PropertyFieldPeoplePicker";
+import { SPHttpClient, SPHttpClientResponse, SPHttpClientConfiguration } from '@microsoft/sp-http';
 
 export interface IPropertyControlsTestWebPartProps {
   people: IPropertyFieldGroupOrPerson[];
@@ -86,29 +89,111 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
     return this.properties.Saved;
   }
 
-  public async render(): Promise<void> {
-    //Test Run--->
-    //var currentPageUrl = this.context.pageContext.web.absoluteUrl + this.context.pageContext.site.serverRequestPath;
+  public async DeleteButton(DeleteButtonValue: any): Promise<any> {
+    this.context.statusRenderer.displayLoadingIndicator(this.domElement, ":Please wait while your Rackhouse is being deleted. Do not close the window or tab");
 
-    // console.log(currentPageUrl);
-    //Get the current timestamp.....
-    this.properties.TimeStamp = new Date().getTime();
-
-    sp.setup({
-      spfxContext: this.context
+    const page = await ClientsidePageFromFile(sp.web.getFileByServerRelativePath("/sites/ModernConnect/SitePages/" + this.properties.RackID + ".aspx"));
+    await page.delete().then(function (response) {
+      console.log("File Deleted !");
+    }).catch(function (exception) {
+      alert("Something went wrong :" + exception);
+    });
+    await sp.web.getFolderByServerRelativePath("Rackhouse Documents/" + this.properties.RackID).delete().then(function (response) {
+      console.log("Folder Deleted !");
+    }).catch(function (exception) {
+      alert("Something went wrong :" + exception);
     });
 
-    try {
-      // Set Image URL received from the file picker component--->
-      const myObj = (this.properties.filePickerResult);
-      // console.log(myObj.fileAbsoluteUrl);
-      this.properties.ImageProperty = myObj.fileAbsoluteUrl;
-    }
-    catch (err) {
+    this.properties.delete = true;
+    this.properties.ParentPageURL = null;
+    this.properties.ImageProperty = null;
+    this.properties.PageTitle = null;
 
-    }
+    this.context.statusRenderer.clearLoadingIndicator(this.domElement);
+    this.render();
 
-    this.domElement.innerHTML = `
+    return
+  }
+
+  public async render(): Promise<void> {
+
+    this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/Web/CurrentUser/Groups`,
+      SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        response.json().then(async (responseJSON: any) => {
+          // console.log(responseJSON.value);
+          var finalArray = responseJSON.value.map(function (obj: { Title: any; }) {
+            return obj.Title;
+          });
+          ///console.log(finalArray);//Array Retrieved from Current users Groups.....
+
+          if (this.properties.people ) {
+            ///console.log(JSON.stringify(this.properties.people));
+
+            const GroupArray = this.properties.people.map((obj: { fullName: any; }) => {
+              return obj.fullName;
+            });
+            var usrFullname = this.context.pageContext.user.displayName;
+            var Groupintersections = finalArray.filter(e => GroupArray.indexOf(e) !== -1);
+            // console.log(Groupintersections)
+
+            ///console.log(GroupArray);//Array Of Group in property pane
+            if (GroupArray.includes(usrFullname) || Groupintersections.length > 0) {
+              // console.log("Current User Present In The Group");
+
+
+
+              if (this.properties.delete == true) {
+
+                this.domElement.innerHTML = `
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+  </head>
+  <div id="LoaderId">
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    
+      <div class="ms-rte-embedcode ms-rte-embedwp" >
+      <div class="${styles.MainContainer}"
+      style="background-image: url(${escape(this.properties.ImageProperty)});
+      box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);
+      background-repeat: no-repeat;width:100%;height:160px;
+      background-size:cover;text-align: center;
+      background-position: center;">
+      <h1>Rackhouse Deleted</h1>
+      <h5 style="color: #cc0a0a;">Please delete this webpart!</h5>
+      
+    </div></div></div>
+    `;
+
+              }
+              else {
+
+
+
+                //Test Run--->
+                //var currentPageUrl = this.context.pageContext.web.absoluteUrl + this.context.pageContext.site.serverRequestPath;
+
+                // console.log(currentPageUrl);
+                //Get the current timestamp.....
+                this.properties.TimeStamp = new Date().getTime();
+
+                sp.setup({
+                  spfxContext: this.context
+                });
+
+                try {
+                  // Set Image URL received from the file picker component--->
+                  const myObj = (this.properties.filePickerResult);
+                  // console.log(myObj.fileAbsoluteUrl);
+                  this.properties.ImageProperty = myObj.fileAbsoluteUrl;
+                }
+                catch (err) {
+
+                }
+
+                this.domElement.innerHTML = `
     <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -118,7 +203,7 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
   
     <div class="ms-rte-embedcode ms-rte-embedwp" >
-    <div class="${styles.RohanContainer}"
+    <div class="${styles.MainContainer}"
     style="background-image: url(${escape(this.properties.ImageProperty)});
     box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%);
     background-repeat: no-repeat;width:100%;height:160px;
@@ -159,46 +244,48 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
     
   </div></div></div>
   `;
-    //  console.log("Saved-"+this.properties.Saved);
+                //  console.log("Saved-"+this.properties.Saved);
 
-    if ((this.properties.Saved != 2) && (this.properties.Saved != 0)) {
-      //Load the webpart on clicking save...
-      this.context.statusRenderer.displayLoadingIndicator(this.domElement, ":Please wait while your Rackhouse is being created. Do not close the window or tab");
+                if ((this.properties.Saved != 2) && (this.properties.Saved != 0)) {
+                  //Load the webpart on clicking save...
+                  this.context.statusRenderer.displayLoadingIndicator(this.domElement, ":Please wait while your Rackhouse is being created. Do not close the window or tab");
 
-      console.log("Folder & Page Creation Started.....");
+                  console.log("Folder & Page Creation Started.....");
 
-      //Creation of A new Folder on saving the webpart...
-      const folderAddResult = await sp.web.rootFolder.folders.getByName("Rackhouse Documents").folders.add("Rack" + this.properties.TimeStamp);
-      //------------>
-      //console.log(folderAddResult.data.ServerRelativeUrl);
-      var folderurl = folderAddResult.data.ServerRelativeUrl;// Get created folders url...
+                  //Creation of A new Folder on saving the webpart...
+                  const folderAddResult = await sp.web.rootFolder.folders.getByName("Rackhouse Documents").folders.add("Rack" + this.properties.TimeStamp);
+                  //------------>
+                  //console.log(folderAddResult.data.ServerRelativeUrl);
+                  var folderurl = folderAddResult.data.ServerRelativeUrl;// Get created folders url...
 
-
-
-
-      //Copy Template Layout and Create a site page...
-      const page = await ClientsidePageFromFile(sp.web.getFileByServerRelativePath("/sites/ModernConnect/SitePages/DND/" + this.properties.Template + ".aspx"));
-      console.log("Selected Page Templete-" + this.properties.Template);
-      // console.log(page);
-
-      // Create a published copy of the page
-      const pageCopy = await page.copy(sp.web, "Rack" + this.properties.TimeStamp, this.properties.PageTitle, false);
-      // pageCopy.addSection().addControl(new ClientsideText("https://devbeam.sharepoint.com" + folderurl)); //Add Text Section--->
-
-      // console.log(folderurl);
-
-      await pageCopy.save();
-
-      console.log("New Page Created By Title: " + pageCopy.title);
+                  console.log("Created page relative url-" + folderurl)
 
 
-      // const pageCopy1 = await CreateClientsidePage(Web("/SitePages/Rackhouse Pages"),this.properties.PageTitle, this.properties.PageTitle);
-      // console.log("New Page Created By Title: " + pageCopy1.title);
+                  //Copy Template Layout and Create a site page...
+                  const page = await ClientsidePageFromFile(sp.web.getFileByServerRelativePath("/sites/ModernConnect/SitePages/DND/" + this.properties.Template + ".aspx"));
+                  console.log("Selected Page Templete-" + this.properties.Template);
+                  // console.log(page);
 
-      this.properties.ParentPageURL = "https://devbeam.sharepoint.com/sites/ModernConnect/SitePages/" + "Rack" + this.properties.TimeStamp + ".aspx";
-      this.context.statusRenderer.clearLoadingIndicator(this.domElement);
+                  // Create a published copy of the page
+                  const pageCopy = await page.copy(sp.web, "Rack" + this.properties.TimeStamp, this.properties.PageTitle, false);
+                  // pageCopy.addSection().addControl(new ClientsideText("https://devbeam.sharepoint.com" + folderurl)); //Add Text Section--->
 
-      this.domElement.innerHTML = `
+                  // console.log(folderurl);
+
+                  await pageCopy.save();
+
+                  console.log("New Page Created By Title: " + pageCopy.title);
+
+
+                  // const pageCopy1 = await CreateClientsidePage(Web("/SitePages/Rackhouse Pages"),this.properties.PageTitle, this.properties.PageTitle);
+                  // console.log("New Page Created By Title: " + pageCopy1.title);
+
+                  this.properties.ParentPageURL = "https://devbeam.sharepoint.com/sites/ModernConnect/SitePages/" + "Rack" + this.properties.TimeStamp + ".aspx";
+                  this.properties.delete = false;
+                  this.context.statusRenderer.clearLoadingIndicator(this.domElement);
+                  this.properties.RackID = "Rack" + this.properties.TimeStamp;
+
+                  this.domElement.innerHTML = `
     <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -208,7 +295,7 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
   
     <div class="ms-rte-embedcode ms-rte-embedwp" >
-    <div class="${styles.RohanContainer}"
+    <div class="${styles.MainContainer}"
     style="background-image: url(${escape(this.properties.ImageProperty)});
     background-repeat: no-repeat;width:100%;height:160px;
     background-size:cover;
@@ -248,18 +335,29 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
     
   </div></div></div>
   `;
-      this.properties.Saved = 0;
+                  this.properties.Saved = 0;
 
-    }
+                }
 
 
-    else {
-      // var PageTitleTemp = this.properties.PageTitle;
-      //console.log("PageTitle-->" + PageTitleTemp);
-      console.log("Click on save to create a Folder and Page...");
-    }
+                else {
+                  // var PageTitleTemp = this.properties.PageTitle;
+                  //console.log("PageTitle-->" + PageTitleTemp);
+                  console.log("Click on save to create a Folder and Page...");
+                }
 
+              }
+            }
+            else {
+              this.domElement.innerHTML = `
+        <div><h5>Permission required to view this webpart!</h5></div>
+      `;
+            }
+          }
+        });
+      });
   }
+
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
@@ -267,6 +365,7 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     let MySaveButton: any;
+    let deleteButton: any;
     if ((this.properties.Saved == 0) || (this.properties.Saved == 1) || (this.properties.Template == null)) {
       MySaveButton = PropertyPaneButton('Button', {
         disabled: true,
@@ -282,6 +381,24 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
         buttonType: PropertyPaneButtonType.Normal,
         onClick: this.SaveButton.bind(this)
       });
+    }
+
+    if (this.properties.delete == true) {
+      deleteButton = PropertyPaneButton('Button', {
+        disabled: true,
+        text: "Delete",
+        buttonType: PropertyPaneButtonType.Primary,
+        onClick: this.DeleteButton.bind(this)
+      });
+    }
+    else {
+      deleteButton = PropertyPaneButton('Button', {
+        disabled: false,
+        text: "Delete",
+        buttonType: PropertyPaneButtonType.Primary,
+        onClick: this.DeleteButton.bind(this)
+      });
+
     }
 
     return {
@@ -329,6 +446,7 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
                   ],
                   //  selectedKey: 'Library',
                 }),
+
                 PropertyPaneDropdown('Language', {
                   label: 'Language',
                   options: [
@@ -390,7 +508,8 @@ export default class PageCreatorWebPart extends BaseClientSideWebPart<IPageCreat
                   key: 'peopleFieldId'
 
                 }),
-                MySaveButton
+                MySaveButton,
+                deleteButton
 
 
 
